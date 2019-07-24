04f4e6c7dc840cd3c1c7f2e0363efb9835c13689
Manikannan Marimuthu <manikannanmarimuthu@gmail.com>
2019-07-24 16:20:57 +0530

    Unwanted Constants removed

--- src/main/java/com/qa/webtable/Constants.java
+++ src/main/java/com/qa/webtable/Constants.java
@@ -1,12 +1,10 @@
 package com.qa.webtable;
 
 public interface Constants {
-	String dummy = "//*[@id=\"customers\"]/tbody/tr[";
 	String breCmnyXpath = "//*[@id=\"customers\"]/tbody/tr[";
 	String AfterCmnyXpath = "]/td[1]";
 	String breContXpath = "//*[@id=\"customers\"]/tbody/tr[";
 	String AfterContXpath = "]/td[2]";
 	String breCounXpath = "//*[@id=\"customers\"]/tbody/tr[";
-	String test = "//*[@id=\"customers\"]/tbody/tr[";
 	String AfterCounXpath = "]/td[3]";
 }
